78c2e8ddac6353beb7f159d77a871a863b1a5245
tyVespA <migliavacca.marco08@gmail.com>
2023-02-12 23:35:46 +0100

    Change files tp .jsx, fix ScrollToTop

--- src/helpers/ScrollToTop.jsx
+++ src/helpers/ScrollToTop.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 const ScrollToTop = (props) => {
   const location = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0);
+    document.body.scrollTo(0, 0);
   }, [location]);
 
   return <>{props.children}</>;
